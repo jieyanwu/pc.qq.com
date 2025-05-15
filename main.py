0ed27a20dc6099b818f0e21c303aac24c45e6065
jie <1048049400@qq.com>
2025-05-15 11:09:57 +0800

    持久化保存数据

--- main.py
+++ main.py
@@ -9,13 +9,19 @@ data = {
     "offset":30,
     "limit":30,
 }
+dic = []
 END_num = 0
-while(END_num<=30):
+while(END_num<=900):
     END_num += 30
     url = BASE_url+"?"+f"c=0&sort=0&offset={END_num}&limit=30"
     print(END_num)
     res = requests.get(url).json()
-    print(res["list"])
+    # print(res["list"])
+    for i in res["list"]:
+        dic.append(i)
+    # dic.append(res["list"])
     # time.sleep(0.2)
-# print(url)
+print(dic)
+with open("data.json","w",encoding="utf-8") as f:
+    json.dump(dic,f,ensure_ascii=False)
 # res = requests.get()
